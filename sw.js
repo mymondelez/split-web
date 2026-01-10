42c26f5841e31f013874660f0c9783f4e5e6ae0f
mymondelez <136745350+mymondelez@users.noreply.github.com>
2026-01-10 17:18:00 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "split2-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
