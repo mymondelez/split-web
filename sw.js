ccabe852afa1b057a5da9407f2f4aa88e05415ce
mymondelez <136745350+mymondelez@users.noreply.github.com>
2026-01-10 20:20:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
-const CACHE = "split2-v1";
+const CACHE = "split2-v2"; // <-- cambia versione (prima era v1)
 const ASSETS = [
   "./",
   "./index.html",
   "./app.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener("fetch", (event) => {
